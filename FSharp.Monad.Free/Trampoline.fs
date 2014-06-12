545364c6050edfc4f72efc684ee85ba1f0babfd9
pocketberserker <pocketberserker@gmail.com>
2014-06-12 21:30:35 +0900

    fix type

--- FSharp.Monad.Free/Trampoline.fs
+++ FSharp.Monad.Free/Trampoline.fs
@@ -4,6 +4,6 @@ type Trampoline<'T> = Free<F0, 'T>
 
 module Trampoline =
 
-  let suspend (f: unit -> Free<F0, 'T>) : Trampoline<_> = f |> F0.ofFunc |> Free.suspend
+  let suspend (f: unit -> Trampoline<'T>) : Trampoline<_> = f |> F0.ofFunc |> Free.suspend
 
   let delay f : Trampoline<_> = suspend (fun () -> Free.done_ (f ()))
