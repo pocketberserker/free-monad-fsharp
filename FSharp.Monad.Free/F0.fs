cb50ebddafdbfce2f8daad99bac1e71db15c01fa
pocketberserker <pocketberserker@gmail.com>
2014-06-12 21:24:51 +0900

    from sealed class to record

--- FSharp.Monad.Free/F0.fs
+++ FSharp.Monad.Free/F0.fs
@@ -2,21 +2,22 @@
 
 type F0 = F0
 
-[<Sealed>]
-type F0<'T>(f: unit -> 'T) =
-  member this.Value = f
-  interface _1<F0, 'T>
+type F0<'T> = {
+  Apply : unit -> 'T
+}
+  with
+    interface _1<F0, 'T>
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module F0 =
 
-  let ofFunc (f: unit -> 'T) = F0(f)
+  let ofFunc f = { Apply = f }
 
-  let toFunc (f: F0<'T>) = f.Value
+  let toFunc f = f.Apply
 
-  let map (f: 'T -> 'U) (f0: F0<'T>) = F0(fun () -> f (f0.Value ()))
+  let map f f0 = { Apply = fun () -> f (f0.Apply ()) }
 
   let functor_ = { new Functor<F0> with
     member this.Map(f, fa: _1<F0, _>) = (fa :?> F0<_>) |> map f :> _1<_, _> }
 
-  let run (x: _1<F0, Free<F0, 'T>>) = (x :?> F0<Free<F0, _>>).Value ()
+  let run (x: _1<F0, Free<F0, 'T>>) = (x :?> F0<Free<F0, _>>).Apply ()
