921f35957cda656b1ccc6342c138bff6b2f7adb4
pocketberserker <pocketberserker@gmail.com>
2014-06-12 09:42:52 +0900

    modify F0 from interface to sealed class

--- FSharp.Monad.Free/F0.fs
+++ FSharp.Monad.Free/F0.fs
@@ -2,20 +2,21 @@
 
 type F0 = F0
 
-type F0<'T> =
-  inherit _1<F0, 'T>
-  abstract member Apply: unit -> 'T
+[<Sealed>]
+type F0<'T>(f: unit -> 'T) =
+  member this.Value = f
+  interface _1<F0, 'T>
 
 [<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
 module F0 =
 
-  let ofFunc (f: unit -> 'T) = { new F0<_> with member this.Apply() = f () }
+  let ofFunc (f: unit -> 'T) = F0(f)
 
-  let toFunc (f: F0<'T>) = fun () -> f.Apply()
+  let toFunc (f: F0<'T>) = f.Value
 
-  let map (f: 'T -> 'U) (f0: F0<'T>) = { new F0<_> with member this.Apply() = f (f0.Apply()) }
+  let map (f: 'T -> 'U) (f0: F0<'T>) = F0(fun () -> f (f0.Value ()))
 
   let functor_ = { new Functor<F0> with
     member this.Map(f, fa: _1<F0, _>) = (fa :?> F0<_>) |> map f :> _1<_, _> }
 
-  let run (x: _1<F0, Free<F0, 'T>>) = (x :?> F0<Free<F0, _>>).Apply()
+  let run (x: _1<F0, Free<F0, 'T>>) = (x :?> F0<Free<F0, _>>).Value ()
