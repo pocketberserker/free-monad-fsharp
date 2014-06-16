226e6ca40d17ed9a0d0962989b3ad41c531c1876
pocketberserker <pocketberserker@gmail.com>
2014-06-17 07:47:00 +0900

    Monad

--- FSharp.Monad.Free/Applicative.fs
+++ FSharp.Monad.Free/Applicative.fs
@@ -1,14 +1,5 @@
 ﻿namespace FSharp.Monad
 
-[<AbstractClass>]
 type Applicative<'F> =
+  inherit Apply<'F>
   abstract member Point : F0<'T> -> _1<'F, 'T>
-  abstract member Ap: F0<_1<'F, F1<'T, 'U>>> * F0<_1<'F, 'T>> -> _1<'F, 'U>
-  abstract member Map: ('T -> 'U) * _1<'F, 'T> -> _1<'F, 'U>
-  default this.Map(F1 f, fa) =
-    let f = F0.ofFunc <| fun () -> this.Point(F0.ofFunc <| fun () -> f)
-    let fa = F0.ofFunc <| fun () -> fa
-    this.Ap(f, fa)
-  interface Apply<'F> with
-    member this.Ap(f, fa) = this.Ap(f, fa)
-    member this.Map(f, fa) = this.Map(f, fa)
---
+++ FSharp.Monad.Free/Monad.fs
@@ -0,0 +1,22 @@
+﻿namespace FSharp.Monad
+
+[<AbstractClass>]
+type Monad<'F> =
+  abstract member Point : F0<'T> -> _1<'F, 'T>
+  abstract member Bind: F1<'A, _1<'F, 'B>> * _1<'F, 'A> -> _1<'F, 'B>
+  abstract member Ap :F0<_1<'F, F1<'A, 'B>>> * F0<_1<'F, 'A>> -> _1<'F, 'B>
+  default this.Ap(f, fa) =
+    let ff = F1.ofFunc <| fun (StdF1 x) -> this.Map(x, fa.Apply())
+    let fa = f.Apply()
+    this.Bind(ff, fa)
+  abstract member Map: ('T -> 'U) * _1<'F, 'T> -> _1<'F, 'U>
+  default this.Map(F1 f, fa) =
+    let f = F1.ofFunc <| fun a -> this.Point(F0.ofFunc <| fun () -> f.Apply(a))
+    this.Bind(f, fa)
+  interface Apply<'F> with
+    member this.Map(f, fa) = this.Map(f, fa)
+    member this.Ap(f, fa) = this.Ap(f, fa)
+  interface Applicative<'F> with
+    member this.Point(f) = this.Point(f)
+  interface Bind<'F> with
+    member this.Bind(f, fa) = this.Bind(f, fa)
