2a566eb1a5fe1b5e5693da45b746a59afe308f98
pocketberserker <pocketberserker@gmail.com>
2014-06-23 00:49:32 +0900

    Coproduct

---
+++ FSharp.Monad.Free/Coproduct.fs
@@ -0,0 +1,20 @@
+﻿namespace FSharp.Monad
+
+type Coproduct = Coproduct
+
+type Coproduct<'F, 'G, 'T> = Coproduct of Choice<_1<'F, 'T>, _1<'G, 'T>>
+  with
+    interface _1<Coproduct, 'T>
+
+[<CompilationRepresentation(CompilationRepresentationFlags.ModuleSuffix)>]
+module Coproduct =
+
+  let map f g func = function
+    | Coproduct (Choice1Of2 l) -> Functor.map f func l |> Choice1Of2 |> Coproduct
+    | Coproduct (Choice2Of2 r) -> Functor.map g func r |> Choice2Of2 |> Coproduct
+
+  let functor_ f g = { new Functor<Coproduct> with
+    member x.Map(func, fa) =
+      fa :?> Coproduct<_, _, _>
+      |> map f g func
+      :> _1<Coproduct, _> }
