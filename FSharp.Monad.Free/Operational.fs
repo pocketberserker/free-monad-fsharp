392bd11a4f3ced250606a13331e65bce10860193
pocketberserker <pocketberserker@gmail.com>
2014-06-28 20:45:38 +0900

    fix require type

--- FSharp.Monad.Free/Operational.fs
+++ FSharp.Monad.Free/Operational.fs
@@ -8,14 +8,14 @@ module Operational =
   let run (interpreter: NT<'S, 'M>) (m: Monad<'M>) (sa: Free<CoYoneda, 'T>) =
     let nt = { new NT<CoYoneda, 'M> with
       member this.Apply(cy) =
-        let co = cy :?> CoYoneda<'S, _>
+        let co = cy :?> CoYoneda<_, _>
         let f = co.Func<'T>()
         let k = interpreter.Apply(co.Value)
         Functor.map m f k
       }
     Free.foldMap nt CoYoneda.functor_<'S, 'T> m sa
 
-  let runId interpreter sa = run interpreter Id.monad sa :?> Id<_> |> Id.run
+  let runId interpreter sa = run interpreter Id.monad sa :?> Id<_, _> |> Id.run
 
   let rec interpret<'F, 'T, 'U, 'V> (returnI: 'T -> 'U)
     (bind: ('T -> 'U) -> 'V -> 'U) (eval: 'F -> 'V) (p: Free<CoYoneda, 'T>) =
